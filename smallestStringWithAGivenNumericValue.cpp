88c22ffcadbf51ddf6f663a90dccea0e6adcc406
Procoder16 <smsoumik1608@gmail.com>
2022-03-22 22:01:16 +0530

    solution to a new question added

---
+++ smallestStringWithAGivenNumericValue.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string res(n,'a');
+        k -= n;
+        
+        while(k > 0){
+            res[--n] += min(25, k);
+            k -= min(25, k);
+        }
+        return res;
+    }
+};
