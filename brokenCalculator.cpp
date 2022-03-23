8dd3f042ecd7ededbcf42772eccad813d3ed03b5
Procoder16 <smsoumik1608@gmail.com>
2022-03-23 21:25:04 +0530

    broken calculator solution added

---
+++ brokenCalculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int result=0;
+        while(target>startValue)
+        {
+            result++;
+            if(target%2==0)
+                target=target/2;
+            else
+                target+=1;
+        }
+        result=result+(startValue-target);
+        return result;
+    }
+};
