cbfa79933b57cf365666b02ad9e82275dde13de8
Procoder16 <smsoumik1608@gmail.com>
2022-03-24 21:40:14 +0530

    count hills and valleys solution added

---
+++ countHillsAndValley.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end()); 
+	    int n = 0;
+	    for(int i = 1; i < nums.size() -1; ++i)
+		    n += (nums[i - 1] > nums[i]) == (nums[i] < nums[i + 1]); 
+        return n;
+    }
+};
