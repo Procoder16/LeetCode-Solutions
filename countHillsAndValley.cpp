14bc9f7e1bfa07da7381105e0028d81442aab704
Procoder16 <smsoumik1608@gmail.com>
2022-03-24 22:28:16 +0530

    comments added for better understanding

--- countHillsAndValley.cpp
+++ countHillsAndValley.cpp
@@ -7,7 +7,10 @@ using namespace std;
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
-        nums.erase(unique(nums.begin(), nums.end()), nums.end()); 
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+            //unique will remove the duplicate elements and then make the unique elements contiguous
+            // then it would return the pointer to the last position uptil where the elements are there
+            //erase will remove the empty space at the end
 	    int n = 0;
 	    for(int i = 1; i < nums.size() -1; ++i)
 		    n += (nums[i - 1] > nums[i]) == (nums[i] < nums[i + 1]); 
