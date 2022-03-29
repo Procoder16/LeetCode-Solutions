e77433aa1f8251d972efcc9344db6f85fe45fbc8
Procoder16 <smsoumik1608@gmail.com>
2022-03-29 19:09:29 +0530

    find the duplicate number solution added

---
+++ findTheDuplicateNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            int index = abs(nums[i]) - 1;
+            nums[index]*=-1;
+            if(nums[index] > 0){
+                return abs(nums[i]);
+            }
+        }
+        
+        return -1;
+    }
+};
