deb2f025d53eb13f89ea9c515a6fa59da6137fd5
Procoder16 <smsoumik1608@gmail.com>
2022-03-30 20:58:34 +0530

    search in a 2d matrix solution added

---
+++ searchInA2DMatrix.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = 0;
+        int n = matrix[0].size() - 1;
+        
+        while(m < matrix.size() && n >= 0){
+            if(matrix[m][n] == target){
+                return true;
+            }
+            else if(matrix[m][n] > target){
+                n--;
+            }
+            else{
+                m++;
+            }
+        }
+        return false;
+    }
+};
