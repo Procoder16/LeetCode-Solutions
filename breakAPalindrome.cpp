c8fc2bde1f16a522ce7e9cbfa37b1d473782fcad
Procoder16 <smsoumik1608@gmail.com>
2022-10-11 23:56:18 +0530

    updated

--- breakAPalindrome.cpp
+++ breakAPalindrome.cpp
@@ -3,8 +3,7 @@ public:
     string breakPalindrome(string pal) {
         int n = pal.length();
         for (int i = 0; i < n / 2; i++) {
-            if (pal[i] != 'a') 
-            {
+            if (pal[i] != 'a') {
                 pal[i] = 'a';
                 return pal ;
             }
