b879adca16ebeac093b9947698d51a6fab047160
Procoder16 <smsoumik1608@gmail.com>
2023-04-03 20:48:05 +0530

    new solution added

---
+++ boatsToSavePeopleII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int countOfBoats = 0;
+        
+        sort(people.begin(), people.end());
+        
+        int low = 0, high = people.size() - 1;
+        
+        while(low <= high){
+            countOfBoats++;
+            if(people[low] + people[high] <= limit){
+                low++;
+            }
+            high--;
+        }
+        return countOfBoats;
+    }
+};
